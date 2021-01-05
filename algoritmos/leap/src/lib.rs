190b4334fea7c0000a7ebb3bc9906761cac42e36
cindRoberta <32280512+cindRoberta@users.noreply.github.com>
2021-01-05 09:28:55 -0300

    Delete lib.rs

--- algoritmos/leap/src/lib.rs
+++
@@ -1,5 +0,0 @@
-pub fn is_leap_year(year: u64) -> bool {
-  //unimplemented!("true if {} is a leap year", year)
-  let year_div = |n: u64| -> bool { year % n == 0 };
-  year_div(4) && !year_div(100) || year_div(400)
-}
