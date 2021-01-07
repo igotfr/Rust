459d0928b1d989347b59e529b6aa4af8257f9d9c
cindRoberta <32280512+cindRoberta@users.noreply.github.com>
2021-01-07 02:12:48 -0300

    Delete lib.rs

--- algoritmos/proverb/src/lib.rs
+++
@@ -1,7 +0,0 @@
-pub fn build_proverb(list: &[&str]) -> String {
-  //unimplemented!("build a proverb from this list of items: {:?}", list)
-  (0..list.len()).map(|i: usize| -> String {
-    if i == list.len() - 1 { format!("And all for the want of a {}.", list[0]) }
-    else { format!("For want of a {} the {} was lost.", list[i], list[i + 1]) }
-  }).collect::<Vec<String>>().join("\n")
-}
